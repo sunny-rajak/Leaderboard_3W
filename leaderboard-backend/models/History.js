fbced0e2ef60331e115e656b939818538022a8d4
sunny-rajak <sunnyrajak257@gmail.com>
2025-07-15 12:54:10 +0530

    Remove all files from Git tracking but keep them locally

--- leaderboard-backend/models/History.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const historySchema = new mongoose.Schema({
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-    points: Number,
-    timestamp: { type: Date, default: Date.now },
-});
-
-module.exports = mongoose.model("History", historySchema);
